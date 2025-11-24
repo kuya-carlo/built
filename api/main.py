bb8892a1d49895261937ebe7c1293b2300bf978e
kuya-carlo <106532351+kuya-carlo@users.noreply.github.com>
2025-11-25 04:56:07 +0800

    feat: added custom openapi

--- api/main.py
+++ api/main.py
@@ -3,6 +3,7 @@ from contextlib import asynccontextmanager
 from fastapi import FastAPI
 from fastapi import HTTPException as HTTPExcept
 from fastapi.exceptions import RequestValidationError
+from fastapi.openapi.utils import get_openapi
 from starlette.exceptions import HTTPException
 
 from src.models import init_db
@@ -28,7 +29,7 @@ material_router = MaterialRouter()
 activity_router = ActivityLogRouter()
 task_router = TaskRouter()
 
-app = FastAPI(lifespan=lifespan)
+app = FastAPI(lifespan=lifespan, title="Built Cost Management API")
 app.add_exception_handler(RequestValidationError, error_handler)
 app.add_exception_handler(HTTPExcept, error_handler)
 app.add_exception_handler(HTTPException, error_handler)
@@ -39,3 +40,19 @@ app.include_router(projects_router)
 app.include_router(material_router)
 app.include_router(activity_router)
 app.include_router(task_router)
+
+
+def custom_openapi():
+    if app.openapi_schema:
+        return app.openapi_schema
+    openapi_schema = get_openapi(
+        title="Built | Cost Management API",
+        version="0.1.1",
+        description="A cost management system for construction companies",
+        routes=app.routes,
+    )
+    app.openapi_schema = openapi_schema
+    return app.openapi_schema
+
+
+app.openapi = custom_openapi
