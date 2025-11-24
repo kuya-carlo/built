03ecfed567dc9ddce19285b40193840f752c7cf6
kuya-carlo <106532351+kuya-carlo@users.noreply.github.com>
2025-11-25 04:50:02 +0800

    feat: linked routers to main

--- api/main.py
+++ api/main.py
@@ -6,7 +6,13 @@ from fastapi.exceptions import RequestValidationError
 from starlette.exceptions import HTTPException
 
 from src.models import init_db
-from src.routes import UserRouter
+from src.routes import (
+    ActivityLogRouter,
+    MaterialRouter,
+    ProjectRouter,
+    TaskRouter,
+    UserRouter,
+)
 from src.utils.errors import error_handler
 
 
@@ -17,6 +23,11 @@ async def lifespan(app: FastAPI):
 
 
 user_router = UserRouter()
+projects_router = ProjectRouter()
+material_router = MaterialRouter()
+activity_router = ActivityLogRouter()
+task_router = TaskRouter()
+
 app = FastAPI(lifespan=lifespan)
 app.add_exception_handler(RequestValidationError, error_handler)
 app.add_exception_handler(HTTPExcept, error_handler)
@@ -24,3 +35,7 @@ app.add_exception_handler(HTTPException, error_handler)
 app.add_exception_handler(Exception, error_handler)
 
 app.include_router(user_router)
+app.include_router(projects_router)
+app.include_router(material_router)
+app.include_router(activity_router)
+app.include_router(task_router)
